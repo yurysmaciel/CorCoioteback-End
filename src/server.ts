bcf07199004af971df3781fbb9f13ab1acaba27c
Yury soares <ymaciel789@gmail.com>
2026-06-23 21:57:04 -0300

    feat: atualizar servidor e adicionar resposta com filtro de entrada

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,36 @@
 import http from "node:http";
 
+const users: {
+	name: string;
+	status: boolean;
+}[] = [
+	{
+		name: "Anderson",
+		status: true,
+	},
+	{
+		name: "Maria",
+		status: true,
+	},
+	{
+		name: "Jose",
+		status: false,
+	},
+];
+
 http
-	.createServer(() => {
-		console.log("HI!!");
+	.createServer((request, response) => {
+		if (request.url === "/users" && request.method === "GET") {
+			response.writeHead(200, {
+				"content-type": "application/json",
+			});
+			response.end(JSON.stringify(users));
+			return;
+		}
+
+		response.writeHead(404, {
+			"content-type": "text/plain",
+		});
+		response.end("Not found!");
 	})
 	.listen(Number(process.env.PORT));
