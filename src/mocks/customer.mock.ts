608808c040f0d03f254573d59b10e53a0e35f7a2
unknown <yurymaciel6371@digitalcollege.internal>
2026-06-30 22:13:28 -0300

    refactor: mover dados simulados para diretorio dedicado

---
+++ src/mocks/customer.mock.ts
@@ -0,0 +1,22 @@
+import type { Customer } from "../types.ts";
+
+export const customers: Customer[] = [
+	{
+		id: 1,
+		name: "Cristiano Ronaldo",
+		email: "cris@email.com",
+		status: true,
+	},
+	{
+		id: 2,
+		name: "Lionel Messi",
+		email: "lionelMessi@gmail.com",
+		status: true,
+	},
+	{
+		id: 3,
+		name: "Neymar Junior",
+		email: "neymarjr@gmail.com",
+		status: false,
+	},
+];
