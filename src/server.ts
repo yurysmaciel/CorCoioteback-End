adc8f42e1a904ef3d4377c526b92116c84f92139
unknown <yurymaciel6371@digitalcollege.internal>
2026-06-30 22:20:35 -0300

    chore: atualizacao automatica

--- src/server.ts
+++ src/server.ts
@@ -1,69 +1,12 @@
-/*
-import http from "node:http";
-
-const users: {
-	name: string;
-	status: boolean;
-}[] = [
-	{
-		name: "Anderson",
-		status: true,
-	},
-	{
-		name: "Maria",
-		status: true,
-	},
-	{
-		name: "Jose",
-		status: false,
-	},
-];
-
-http
-	.createServer((request, response) => {
-		if (request.url === "/users" && request.method === "GET") {
-			response.writeHead(200, {
-				"content-type": "application/json",
-			});
-			response.end(JSON.stringify(users));
-			return;
-		}
-
-		response.writeHead(404, {
-			"content-type": "text/plain",
-		});
-		response.end("Not found!");
-	})
-	.listen(Number(process.env.PORT));
-
-*/
-
 import express from "express";
+import { customers } from "./mocks/customer.mock.ts";
 
 const app = express();
 
-const users: {
-	name: string;
-	status: boolean;
-}[] = [
-	{
-		name: "Anderson",
-		status: true,
-	},
-	{
-		name: "Maria",
-		status: true,
-	},
-	{
-		name: "Jose",
-		status: false,
-	},
-];
-
 app.use(express.json());
 
-app.get("/users", (_request, response) => {
-	response.json(users);
+app.get("/customers", (_request, response) => {
+	response.json(customers);
 });
 
 app.listen(Number(process.env.PORT));
