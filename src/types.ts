59f7ef072302c19bd4dd2d8640283aa1310ae2e2
yurysmaciel <ymaciel789@gmail.com>
2026-07-10 20:06:58 -0300

    feat: Adicao do novo type invoice

--- src/types.ts
+++ src/types.ts
@@ -5,6 +5,18 @@ export type Customer = {
 	status: boolean;
 };
 
+export type Invoice = {
+	id: Number;
+	value: String;
+	customer_id: Number;
+	status: "pending"  | "paid";
+	create_AT: String;
+}
+
+export type CreateInvoice = Omit<Invoice, "id" | "status">;
+type InvoiceWithoutId = Omit<Invoice, "id">;
+export type UpdateInvoice = Partial<InvoiceWithoutId>;
+
 export type CreateCustomer = Omit<Customer, "id" | "status">;
 type CustomerWithoutId = Omit<Customer, "id">;
 export type UpdateCustomer = Partial<CustomerWithoutId>;
