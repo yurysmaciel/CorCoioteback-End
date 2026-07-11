7b4170f69469259d0ed48ef56829e4aa63ea3bdd
yurysmaciel <ymaciel789@gmail.com>
2026-07-10 21:19:52 -0300

    feat: criacao dos dados mock de invoice

---
+++ src/mocks/invoice.mock.ts
@@ -0,0 +1,25 @@
+import type { Invoice } from "../types.ts";
+
+export const invoices: Invoice[] = [
+    {
+        id: 1,
+        value: "7000",
+        customer_id: 2,
+        status: "paid",
+        create_AT: "10/07/2026",
+    },
+    {
+        id: 1,
+        value: "10000",
+        customer_id: 1,
+        status: "paid",
+        create_AT: "10/07/2026",
+    },
+    {
+        id: 1,
+        value: "2000",
+        customer_id: 3,
+        status: "paid",
+        create_AT: "10/07/2026",
+    },
+];
