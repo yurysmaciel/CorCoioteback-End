d223611e5d1bf44c6e7c703859b067cf514e267e
yurysmaciel <ymaciel789@gmail.com>
2026-07-12 17:23:42 -0300

    feat: adicao server invoice

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import CustomerRouter from "./routes/customer.routes.ts";
+import InvoiceRouter from "./routes/invoice.routes.ts";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/customers", CustomerRouter);
+app.use("/invoices", InvoiceRouter);
 
 app.use((_request, response) => {
 	response.status(404).json({
