1ff3f6b689c52c5f789a3a35ec6833956df38098
unknown <yurymaciel6371@digitalcollege.internal>
2026-07-21 19:57:06 -0300

    feat: adicionar intermediario para triagem de requisicoes

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import requestLogger from "./middlewares/requestLogger.ts";
 import CustomerRouter from "./routes/customer.routes.ts";
 import InvoiceRouter from "./routes/invoice.routes.ts";
 
 const app = express();
+
+app.use(requestLogger);
 
 app.use(express.json());
 
