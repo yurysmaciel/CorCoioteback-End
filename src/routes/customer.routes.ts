af26ba15b89dd6fa7996598426411cefde39bfe1
unknown <yurymaciel6371@digitalcollege.internal>
2026-07-28 22:26:31 -0300

    feat: adicionar intermediario para validacao de dados da requisicao

--- src/routes/customer.routes.ts
+++ src/routes/customer.routes.ts
@@ -1,12 +1,25 @@
 import { Router } from "express";
 import * as CustomerController from "../controllers/customer.controller.ts";
+import validate from "../middlewares/validate.ts";
+import {
+	createCustomerSchema,
+	updateCustomerSchema,
+} from "../schemas/customer.schemas.ts";
 
 const router = Router();
 
 router.get("/", CustomerController.getAllCustomer);
 router.get("/:id", CustomerController.getCustomerById);
-router.post("/", CustomerController.createCustomer);
-router.put("/:id", CustomerController.updateCustomer);
+router.post(
+	"/",
+	validate(createCustomerSchema),
+	CustomerController.createCustomer,
+);
+router.put(
+	"/:id",
+	validate(updateCustomerSchema),
+	CustomerController.updateCustomer,
+);
 router.delete("/:id", CustomerController.deleteCustomer);
 
 export default router;
